e78113d44af17863ccd4921a5a563b107f62c1b1
bomby747 <amijo998@ajou.ac.kr>
2021-11-04 02:16:41 -0700

    20211104

---
+++ num2English.py
@@ -0,0 +1,14 @@
+def num2English(num):
+    num_dict={
+                '1':'one',
+                '2':'two',
+                '3':'three',
+                '4':'four',
+                '5':'five',
+                '6':'six',
+                '7':'seven',
+                '8':'eight',
+                '9':'nine'
+               }
+    print(num_dict[str(num)])
+    
